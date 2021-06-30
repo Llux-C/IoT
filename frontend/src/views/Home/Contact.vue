3c48e4ab3c4a0692b79f671aa9c7fe394634c553
Llux-C <244421824@qq.com>
2021-06-30 18:41:48 +0800

    6.30

---
+++ frontend/src/views/Home/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contact" style="minheight: calc(100vh - 139px)">
+    <a-card hoverable style="width: 100%" :bordered="false">
+      <a-card-meta title="蔡灿宇 //Lluvia" description="3180101972@zju.edu.cn">
+        <a-avatar
+          slot="avatar"
+          src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+          :size="64"
+        />
+      </a-card-meta>
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  components: {},
+};
+</script>
---
+++ frontend/src/views/Home/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="help" style="minHeight:calc(100vh - 139px)">
+    <a-card hoverable style="width: 100%" :bordered="false">
+      <a-card-meta title="操作问题详见用户手册" description="遇到解决不了的问题请点击“联系我们”">
+      </a-card-meta>
+    </a-card>
+    <a-card hoverable style="width: 100%" :bordered="false">
+      <a-card-meta title="运行问题详见运行开发指南手册" description="遇到解决不了的问题请点击“联系我们”">
+      </a-card-meta>
+    </a-card>
+    <a-card hoverable style="width: 100%" :bordered="false">
+      <a-card-meta title="欢迎提出PR，详见接口文档手册" description="遇到解决不了的问题请点击“联系我们”">
+      </a-card-meta>
+    </a-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Help',
+  components: {
+  }
+}
+</script>
